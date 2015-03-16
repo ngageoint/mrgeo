7f5654ff5413e4efae9af72a7872a499e245c0fa
Tim Tisler <tim.tisler@digitalglobe.com>
2015-03-16 10:20:53 -0400

    Added reflection to support Spark 1.0.0 build

--- mrgeo-core/src/main/scala/org/mrgeo/spark/JavaSerializer.scala
+++ mrgeo-core/src/main/scala/org/mrgeo/spark/JavaSerializer.scala
@@ -118,7 +118,14 @@ class JavaSerializer(conf: SparkConf) extends org.apache.spark.serializer.Serial
   }
 
   override def newInstance(): SerializerInstance = {
-    val classLoader = defaultClassLoader.getOrElse(Thread.currentThread.getContextClassLoader)
+    // defaultClassloader is missing from org.apache.spark.serializer.Serializer in Spark 1.0.0, so
+    // we use reflection to try to load it, then fallback is we get an exception.  This mimics the
+    // behavior if the member exists
+    var classLoader = Thread.currentThread.getContextClassLoader
+    try {
+      val cl = getClass.getField("defaultClassLoader").asInstanceOf[Option[ClassLoader]]
+      classLoader = cl.getOrElse(Thread.currentThread.getContextClassLoader)
+    }
     new JavaSerializerInstance(counterReset, classLoader)
   }
 
