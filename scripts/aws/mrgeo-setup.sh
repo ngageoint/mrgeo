d927d5f62691430cfbd649538290fccfa2a8fe02
Tim Tisler <tim.tisler@digitalglobe.com>
2015-08-14 10:22:05 -0400

    Renamed ec2 directory to aws.  Added aws-start and aws-stop scripts

---
+++ scripts/aws/mrgeo-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+aws s3 cp s3://mrgeodev/mrgeo-0.5.0-2.4.0-SNAPSHOT-2.4.0.tar.gz /home/hadoop
+mkdir /home/hadoop/mrgeo
+cd /home/hadoop/mrgeo
+tar -xvf /home/hadoop/mrgeo-0.5.0-2.4.0-SNAPSHOT-2.4.0.tar.gz
+export MRGEO_HOME=/home/hadoop/mrgeo
+hadoop fs -mkdir /mrgeo
+hadoop fs -mkdir /mrgeo/images
+hadoop fs -mkdir /mrgeo/images/color-scales
+hadoop fs -put /home/hadoop/mrgeo/color-scales/* /mrgeo/color-scales
+
