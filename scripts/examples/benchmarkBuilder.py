5bfe93296e3ef8f917c7c673d6a7619e40e5f533
Nathan Campbell <campnatt@gmail.com>
2015-08-31 10:16:43 -0400

    Add option for overriding auto-terminate

--- scripts/examples/benchmarkBuilder.py
+++ scripts/examples/benchmarkBuilder.py
@@ -12,13 +12,20 @@ parser.add_argument("bidprice", help="AWS spot bid price.", action="store")
 parser.add_argument("-n", "--nodetype", help="AWS instance type, m3.xlarge is default.", action="store", default="m3.xlarge")
 parser.add_argument("-z", "--zone", help="AWS availability zone, us-east-1a is default.", action="store", default="us-east-1a")
 parser.add_argument("scratch", help="Run the full suite of benchmarks (full) or run individual ones (partial). Will take a very long time for full. If partial please specify which benchmarks to run.", choices=['full','partial'], action="store", default="partial")
-parser.add_argument("-ii", "--ingestimage", help="Benchmark MrGeo IngestImage. Use if you wish to benchmark ingest.", action="store_true")
-parser.add_argument("-rv", "--rasterizevector", help="Benchmark Rasterize Vector. Use if you wish to benchmark rasterize vector.", action="store_true")
-parser.add_argument("-bp", "--buildpyramid", help="MrGeo BuildPyramid. Use if you wish to benchmark building pyramids.", action="store_true")
-parser.add_argument("-sl", "--slope", help="MrGeo MapAlgebra Slope. Use to benchmark slope calculation.", action="store_true")
-parser.add_argument("-lc", "--landcover", help="MrGeo MapAlgebra Land Cover processing. Use to benchmark nested conditional statment.", action="store_true")
-parser.add_argument("-fs", "--frictionsurface", help="MrGeo MapAlgebra friction surface processing.", action="store_true")
+parser.add_argument("-x", "--noterminate", help="Do not automatically terminate the cluster when all steps have completed. Default behavior is to auto terminate. Use this flag to override.", action="store_true")
+parser.add_argument("-ii", "--ingestimage", help="Benchmark ingest image.", action="store_true")
+parser.add_argument("-rv", "--rasterizevector", help="Benchmark rasterize vector.", action="store_true")
+parser.add_argument("-bp", "--buildpyramid", help="Benchmark build pyramid.", action="store_true")
+parser.add_argument("-sl", "--slope", help="Benchmark slope calculation.", action="store_true")
+parser.add_argument("-lc", "--landcover", help="Benchmark nested conditional statment converting land cover data to impedance values.", action="store_true")
+parser.add_argument("-fs", "--frictionsurface", help="Benchmark friction surface map algebra.", action="store_true")
 args = parser.parse_args()
+
+# Set auto-terminate option
+if args.noterminate:
+  nt = "--no-auto-terminate"
+else:
+  nt = "--auto-terminate"
 
 # Create cluster based on user input
 if args.scratch == 'full':
@@ -28,7 +35,7 @@ if args.scratch == 'full':
 	--ec2-attributes KeyName=' + args.userkey + ',AvailabilityZone=' + args.zone + ' \
 	--log-uri "s3://mrgeo-deploy/logs" \
 	--instance-groups InstanceCount=1,Name=Master,InstanceGroupType=MASTER,InstanceType=' + args.nodetype + ' InstanceCount=' + args.numnodes + ',Name=Core,InstanceGroupType=CORE,InstanceType=' + args.nodetype + ',BidPrice=' + args.bidprice + ' \
-	--tags mrgeobenchmark= --auto-terminate --no-termination-protected \
+	--tags mrgeobenchmark= ' + nt + ' --no-termination-protected \
 	--bootstrap-actions \
 	Path=s3://mrgeo-deploy/mrgeo-emr-bootstrap.sh,Name="Fix Up EMR for MrGeo" \
 	Path=s3://mrgeo-deploy/install-gdal.sh,Name="Install GDAL" \
@@ -74,7 +81,7 @@ else:
 		--ec2-attributes KeyName=' + args.userkey + ',AvailabilityZone=' + args.zone + ' \
 		--log-uri "s3://mrgeo-deploy/logs" \
 		--instance-groups InstanceCount=1,Name=Master,InstanceGroupType=MASTER,InstanceType=' + args.nodetype + ' InstanceCount=' + args.numnodes + ',Name=Core,InstanceGroupType=CORE,InstanceType=' + args.nodetype + ',BidPrice=' + args.bidprice + ' \
-		--tags mrgeobenchmark= --auto-terminate --no-termination-protected \
+		--tags mrgeobenchmark= ' + nt + ' --no-termination-protected \
 		--bootstrap-actions \
 		Path=s3://mrgeo-deploy/mrgeo-emr-bootstrap.sh,Name="Fix Up EMR for MrGeo" \
 		Path=s3://mrgeo-deploy/install-gdal.sh,Name="Install GDAL" \
@@ -90,5 +97,3 @@ else:
 	  	print "\n---------------------------------------\nSpot request successful.\nYour cluster ID is: " + proc + "Your cluster name is: MrGeo Benchmark Cluster - " + args.userkey + ' - ' + nowf + "\n---------------------------------------\nCheck cluster status by running aws emr list-instances --cluster-id " + proc
 	  except subprocess.CalledProcessError:
 	  	print "Error starting cluster!"
-
-
