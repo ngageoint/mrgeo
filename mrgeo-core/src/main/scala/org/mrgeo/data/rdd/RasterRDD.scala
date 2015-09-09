3e65e48afc9d5ea5ec070d40fa625186245117a5
Tim Tisler <tim.tisler@digitalglobe.com>
2015-09-09 08:09:47 -0400

    Converting old parsing

---
+++ mrgeo-core/src/main/scala/org/mrgeo/data/rdd/RasterRDD.scala
@@ -0,0 +1,8 @@
+package org.mrgeo.data.rdd
+
+import org.mrgeo.data.raster.RasterWritable
+import org.mrgeo.data.tile.TileIdWritable
+
+class RasterRDD extends MrGeoRDD[TileIdWritable, RasterWritable] {
+
+}
--- mrgeo-core/src/main/scala/org/mrgeo/mapalgebra/MapOp.scala
+++ mrgeo-core/src/main/scala/org/mrgeo/mapalgebra/MapOp.scala
@@ -1,5 +1,4 @@
 package org.mrgeo.mapalgebra
 
 abstract class MapOp {
- protected def register:Array[String]
 }
