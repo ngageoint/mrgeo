7b269622f1523975bd8a4fe5a1256203312b7263
Tim Tisler <tim.tisler@digitalglobe.com>
2015-09-23 10:07:54 -0400

    Added shell of LeastCostPathMapOp

---
+++ mrgeo-mapalgebra/mrgeo-mapalgebra-costdistance/src/main/scala/org/mrgeo/mapalgebra/LeastCostPathMapOp.scala
@@ -0,0 +1,31 @@
+package org.mrgeo.mapalgebra
+
+import java.io.{ObjectInput, ObjectOutput, Externalizable}
+
+import org.apache.spark.{SparkContext, SparkConf}
+import org.mrgeo.mapalgebra.old.MapOpRegistrar
+import org.mrgeo.mapalgebra.parser.ParserNode
+import org.mrgeo.spark.job.JobArguments
+
+object LeastCostPathMapOp extends MapOpRegistrar {
+  override def register: Array[String] = {
+    Array[String]("leastCostPath", "lcp")
+  }
+
+  override def apply(node:ParserNode, variables: String => Option[ParserNode]): MapOp =
+    new LeastCostPathMapOp(node, variables)
+}
+
+// TODO:  Complete this when VectorMapOp is complete
+class LeastCostPathMapOp extends MapOp with Externalizable {
+
+  private[mapalgebra] def this(node: ParserNode, variables: String => Option[ParserNode]) = {
+    this()
+  }
+
+  override def setup(job: JobArguments, conf: SparkConf): Boolean = true
+  override def execute(context: SparkContext): Boolean = true
+  override def teardown(job: JobArguments, conf: SparkConf): Boolean = true
+  override def readExternal(in: ObjectInput): Unit = {}
+  override def writeExternal(out: ObjectOutput): Unit = {}
+}
