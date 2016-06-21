c18054769654d13210d41491cb6105fecc747214
ericwood73 <ericwood73@gmail.com>
2016-06-21 17:04:52 -0400

    Added Test for ImageSplitGenerator
    Also removed some unused imports and an used variable.
    
    Signed-off-by: ericwood73 <ericwood73@gmail.com>

--- mrgeo-core/src/main/java/org/mrgeo/hdfs/output/image/HdfsMrsPyramidOutputFormat.java
+++ mrgeo-core/src/main/java/org/mrgeo/hdfs/output/image/HdfsMrsPyramidOutputFormat.java
@@ -47,7 +47,6 @@ public class HdfsMrsPyramidOutputFormat extends MapFileOutputFormat
     // this RecordWriter is copied from the MapFileOutputFormat, with the addition of
     // the write() method checking for TileIdWritable.  We needed to copy the code
     // because the RecordWriter is actually inline.  yuck!
-    Configuration conf = context.getConfiguration();
     CompressionCodec codec = null;
     SequenceFile.CompressionType compressionType = SequenceFile.CompressionType.NONE;
     if (getCompressOutput(context)) {
@@ -65,7 +64,7 @@ public class HdfsMrsPyramidOutputFormat extends MapFileOutputFormat
     return new Writer(out);
   }
 
-  MapFile.Writer createMapFileWriter(TaskAttemptContext context, CompressionCodec codec,
+  protected MapFile.Writer createMapFileWriter(TaskAttemptContext context, CompressionCodec codec,
                                              SequenceFile.CompressionType compressionType, Path file) throws IOException {
     return new MapFile.Writer(context.getConfiguration(), file,
         MapFile.Writer.keyClass(context.getOutputKeyClass().asSubclass(WritableComparable.class)),
@@ -74,7 +73,7 @@ public class HdfsMrsPyramidOutputFormat extends MapFileOutputFormat
         MapFile.Writer.progressable(context));
   }
 
-  CompressionCodec getCompressionCodec(TaskAttemptContext context) {
+  protected CompressionCodec getCompressionCodec(TaskAttemptContext context) {
       CompressionCodec codec;
       Class<?> codecClass = getOutputCompressorClass(context,
           DefaultCodec.class);
