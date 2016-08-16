578c0fabf0f15d5524cc4095a821e46520b09e99
Dave Johnson <david.johnson@digitalglobe.com>
2016-08-16 11:23:19 -0400

    Update python aspect map algebra function to accept a value to use for flat pixels
     - closes #352
     - updated/added tests accordingly

--- mrgeo-mapalgebra/mrgeo-mapalgebra-terrain/src/main/java/org/mrgeo/mapalgebra/AspectMapOp.scala
+++ mrgeo-mapalgebra/mrgeo-mapalgebra-terrain/src/main/java/org/mrgeo/mapalgebra/AspectMapOp.scala
@@ -16,6 +16,8 @@
 
 package org.mrgeo.mapalgebra
 
+import java.io.{ObjectInput, ObjectOutput}
+
 import org.mrgeo.mapalgebra.parser.{ParserException, ParserNode}
 import org.mrgeo.mapalgebra.raster.RasterMapOp
 import org.mrgeo.utils.FloatUtils
@@ -25,8 +27,8 @@ object AspectMapOp extends MapOpRegistrar {
     Array[String]("aspect")
   }
 
-  def create(raster:RasterMapOp, units:String="rad"):MapOp = {
-    new AspectMapOp(Some(raster), units)
+  def create(raster:RasterMapOp, units:String="rad", flatValue:Double= -1.0):MapOp = {
+    new AspectMapOp(Some(raster), units, flatValue)
   }
 
   override def apply(node:ParserNode, variables: String => Option[ParserNode]): MapOp =
@@ -37,10 +39,13 @@ class AspectMapOp extends SlopeAspectMapOp {
   final val TWO_PI:Double = 2 * Math.PI
   final val THREE_PI_OVER_2:Double = (3.0 * Math.PI) / 2.0
 
-  private[mapalgebra] def this(inputMapOp:Option[RasterMapOp], units:String) = {
+  var flatValue: Double = -1.0
+
+  private[mapalgebra] def this(inputMapOp:Option[RasterMapOp], units:String, flatValue:Double) = {
     this()
 
     initialize(inputMapOp, units)
+    this.flatValue = flatValue
   }
 
   private[mapalgebra] def this(node:ParserNode, variables: String => Option[ParserNode]) = {
@@ -49,8 +54,8 @@ class AspectMapOp extends SlopeAspectMapOp {
     if (node.getNumChildren < 1) {
       throw new ParserException(node.getName + " requires at least one argument")
     }
-    else if (node.getNumChildren > 2) {
-      throw new ParserException(node.getName + " requires only one or two arguments")
+    else if (node.getNumChildren > 4) {
+      throw new ParserException(node.getName + " takes no more than four arguments")
     }
 
     val inputMapOp = RasterMapOp.decodeToRaster(node.getChild(0), variables)
@@ -62,24 +67,39 @@ class AspectMapOp extends SlopeAspectMapOp {
       }
     }
     initialize(inputMapOp, units)
+    if (node.getNumChildren >= 3) {
+      flatValue = MapOp.decodeDouble(node.getChild(2)) match {
+        case Some(d) => d
+        case _ => throw new ParserException("Error decoding double from " + node.getChild(2).getName)
+      }
+    }
   }
 
   override def computeTheta(normal: (Double, Double, Double)): Double = {
     // if the z component of the normal is 1.0, the cell is flat, so the aspect is undefined.
     // For now, we'llset it to 0.0, but another value could be more appropriate.
     if (FloatUtils.isEqual(normal._3, 1.0)) {
-      0.0
+      flatValue
     }
     else {
       // change from (-Pi to Pi) to ( [0 to 2Pi) ), make 0 deg north (+ 3pi/2)
       // convert to clockwise
       val t = TWO_PI - (Math.atan2(normal._2, normal._1) + THREE_PI_OVER_2) % TWO_PI
       if (FloatUtils.isEqual(t, TWO_PI)) {
-        0.0
+        flatValue
       }
       else {
         t
       }
     }
+  }
+  override def readExternal(in: ObjectInput): Unit = {
+    super.readExternal(in)
+    flatValue = in.readDouble()
+  }
+
+  override def writeExternal(out: ObjectOutput): Unit = {
+    super.writeExternal(out)
+    out.writeDouble(flatValue)
   }
 }
