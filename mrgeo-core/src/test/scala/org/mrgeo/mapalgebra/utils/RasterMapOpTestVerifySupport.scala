c4d070d454292ee62ff197f92792fd9744e1aae8
Tim Tisler <tim.tisler@digitalglobe.com>
2016-09-21 13:27:27 -0400

    Convert RasterMapOpBuilder to mrspyramid-v3

--- mrgeo-core/src/test/scala/org/mrgeo/mapalgebra/utils/RasterMapOpTestVerifySupport.scala
+++ mrgeo-core/src/test/scala/org/mrgeo/mapalgebra/utils/RasterMapOpTestVerifySupport.scala
@@ -1,8 +1,8 @@
 package org.mrgeo.mapalgebra.utils
 
-import java.awt.image.Raster
+
 import org.apache.spark.rdd.RDD
-import org.mrgeo.data.raster.RasterWritable
+import org.mrgeo.data.raster.{MrGeoRaster, RasterWritable}
 import org.mrgeo.data.tile.TileIdWritable
 import org.mrgeo.utils.tms.TMSUtils
 import org.scalatest.Assertions
@@ -12,7 +12,7 @@ import org.scalatest.Assertions
   */
 trait RasterMapOpTestVerifySupport extends RasterMapOpTestSupport {
 
-  type RasterVerifier = (Raster) => Unit
+  type RasterVerifier = (MrGeoRaster) => Unit
   type RasterVerifiers = Map[Long, RasterVerifier]
 
   def Assertions = new Object with Assertions
@@ -21,17 +21,15 @@ trait RasterMapOpTestVerifySupport extends RasterMapOpTestSupport {
   // the RDD, then the method should fail the test if failOnMissingKey is true
   def verifyRasters(rdd: RDD[_], verifiers: RasterVerifiers, failOnMissingKey: Boolean = true): Unit = {
     var idsToVerify = verifiers.keySet
-    rdd.toLocalIterator.foreach(element => {
-      element match {
-        case (tileId: TileIdWritable, raster: RasterWritable) => verifiers.get(tileId.get()) match {
-          case Some(verifier) => {
-            verifier(RasterWritable.toRaster(raster))
-            // Record the key
-            idsToVerify = idsToVerify - tileId.get()
-          }
-        }
+    rdd.toLocalIterator.foreach {
+      case (tileId: TileIdWritable, raster: RasterWritable) => verifiers.get(tileId.get()) match {
+      case Some(verifier) =>
+        verifier(RasterWritable.toMrGeoRaster(raster))
+        // Record the key
+        idsToVerify = idsToVerify - tileId.get()
       }
-    })
+      case None =>
+    }
     if (failOnMissingKey && idsToVerify.size != 0) Assertions.fail("Keys missing from RDD: " + idsToVerify.mkString)
   }
 
@@ -55,26 +53,26 @@ trait RasterMapOpTestVerifySupport extends RasterMapOpTestSupport {
     * @param expectedData
     */
   def verifyRastersNoData(rdd: RDD[_], tileIds: Array[Long], tileSize: Int, zoomLevel: Int, nodatas: Array[Double],
-                          left: Double, right: Double, top: Double, bottom: Double,
-                          expectedData: Option[Array[Double]] = None) = {
-    val verifier = (tid: Long, r: Raster) =>
+      left: Double, right: Double, top: Double, bottom: Double,
+      expectedData: Option[Array[Double]] = None) = {
+    val verifier = (tid: Long, r: MrGeoRaster) =>
       verifyRasterNoData(tid, zoomLevel, tileSize, r, nodatas, left, right, top, bottom, expectedData)
-    val verifiers = tileIds.map(t => (t, (r: Raster) => verifier(t, r))).toMap
+    val verifiers = tileIds.map(t => (t, (r: MrGeoRaster) => verifier(t, r))).toMap
     verifyRasters(rdd, verifiers)
   }
 
-  def verifyRastersAreTheSame(expected: Raster)(actual: Raster): Unit = {
+  def verifyRastersAreTheSame(expected: MrGeoRaster)(actual: MrGeoRaster): Unit = {
     // Assert rasters are the same size
-    val width = expected.getWidth();
-    val height = expected.getHeight();
-    val bands = expected.getNumBands();
+    val width = expected.width()
+    val height = expected.height()
+    val bands = expected.bands()
 
-    Assertions.assertResult(width, "Raster width") {actual.getWidth()}
-    Assertions.assertResult(height, "Raster height") {actual.getHeight()}
-    Assertions.assertResult(bands, "Raster number of bands") {actual.getNumBands()}
+    Assertions.assertResult(width, "Raster width") {actual.width()}
+    Assertions.assertResult(height, "Raster height") {actual.height()}
+    Assertions.assertResult(bands, "Raster number of bands") {actual.bands()}
 
     forEachSampleInRaster(actual, (b, x, y, sample) => {
-      Assertions.assertResult(expected.getSample(x, y, b), s"Sample at x: $x y: $y band: $b") {sample}
+      Assertions.assertResult(expected.getPixelDouble(x, y, b), s"Sample at x: $x y: $y band: $b") {sample}
     })
   }
 
@@ -89,8 +87,8 @@ trait RasterMapOpTestVerifySupport extends RasterMapOpTestSupport {
     * @param top
     * @param bottom
     */
-  def verifyRasterNoData(tileId: Long, zoomLevel: Int, tileSize: Int, raster: Raster, nodatas: Array[Double],
-                         left: Double, right: Double, top: Double, bottom: Double, expectedData: Option[Array[Double]]) = {
+  def verifyRasterNoData(tileId: Long, zoomLevel: Int, tileSize: Int, raster: MrGeoRaster, nodatas: Array[Double],
+      left: Double, right: Double, top: Double, bottom: Double, expectedData: Option[Array[Double]]) = {
 
     // Get the tile for the id and zoom level
     val tile = TMSUtils.tileid(tileId, zoomLevel)
@@ -100,19 +98,19 @@ trait RasterMapOpTestVerifySupport extends RasterMapOpTestSupport {
     println(s"For Tile $tileId LeftBottom bounds pixel: ${pixelLeftBottom.px}, ${pixelLeftBottom.py}. RightTop bounds pixel: ${pixelRightTop.px}, ${pixelRightTop.py}")
 
     val verifyExpectedData = expectedData match {
-      case Some(dataArray) => (b: Int, x: Int, y: Int, sample: Double) => Unit
-        if (b < dataArray.size) Assertions.assertResult(dataArray(b), s"Sample at x: $x y: $y band: $b") {
-          sample
-        }
-      case None => (b: Int, x: Int, y: Int, sample: Double) => Unit
+    case Some(dataArray) => (b: Int, x: Int, y: Int, sample: Double) => Unit
+      if (b < dataArray.size) Assertions.assertResult(dataArray(b), s"Sample at x: $x y: $y band: $b") {
+        sample
+      }
+    case None => (b: Int, x: Int, y: Int, sample: Double) => Unit
     }
 
     def verifySample(b: Int, x: Int, y: Int, sample: Double): Unit = {
       (x, y) match {
-        case (x, y) if x < pixelLeftBottom.px || x > pixelRightTop.px ||
+      case (x, y) if x < pixelLeftBottom.px || x > pixelRightTop.px ||
           y < pixelLeftBottom.py || y > pixelRightTop.py =>
-          Assertions.assertResult(nodatas(b), s"Sample at x: $x y: $y band: $b") {sample}
-        case _ => verifyExpectedData(b, x, y, sample)
+        Assertions.assertResult(nodatas(b), s"Sample at x: $x y: $y band: $b") {sample}
+      case _ => verifyExpectedData(b, x, y, sample)
       }
     }
 
@@ -125,10 +123,10 @@ trait RasterMapOpTestVerifySupport extends RasterMapOpTestSupport {
     * @param raster
     * @param f function that takes band, x, y, and sample
     */
-  def forEachSampleInRaster(raster: Raster, f: (Int, Int, Int, Double) => Unit) = {
-    val width = raster.getWidth();
-    val height = raster.getHeight();
-    val bands = raster.getNumBands();
+  def forEachSampleInRaster(raster: MrGeoRaster, f: (Int, Int, Int, Double) => Unit) = {
+    val width = raster.width()
+    val height = raster.height()
+    val bands = raster.bands()
     println(s"Raster height: $height width: $width bands: $bands")
 
     // Loop over every sample.
@@ -136,7 +134,7 @@ trait RasterMapOpTestVerifySupport extends RasterMapOpTestSupport {
       b <- 0 to bands - 1
       y <- 0 to height - 1
       x <- 0 to width - 1} {
-      f(b, x, y, raster.getSampleDouble(x, y, b))
+      f(b, x, y, raster.getPixelDouble(x, y, b))
     }
   }
 }
