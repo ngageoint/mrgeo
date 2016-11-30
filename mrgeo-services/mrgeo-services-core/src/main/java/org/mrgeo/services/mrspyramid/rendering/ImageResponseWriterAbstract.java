d4949000218c1958881fb30d4d48335d0e6d736f
Tim Tisler <tim.tisler@digitalglobe.com>
2016-11-30 15:12:21 -0500

    Correct sonar issues

--- mrgeo-services/mrgeo-services-core/src/main/java/org/mrgeo/services/mrspyramid/rendering/ImageResponseWriterAbstract.java
+++ mrgeo-services/mrgeo-services-core/src/main/java/org/mrgeo/services/mrspyramid/rendering/ImageResponseWriterAbstract.java
@@ -16,12 +16,8 @@
 
 package org.mrgeo.services.mrspyramid.rendering;
 
-import org.mrgeo.data.DataProviderFactory;
-import org.mrgeo.data.ProviderProperties;
-import org.mrgeo.data.image.MrsImageDataProvider;
 import org.mrgeo.data.raster.MrGeoRaster;
 import org.mrgeo.image.MrsPyramid;
-import org.mrgeo.image.MrsPyramidMetadata;
 import org.mrgeo.services.ServletUtils;
 import org.mrgeo.utils.tms.Bounds;
 import org.slf4j.Logger;
@@ -57,11 +53,9 @@ public abstract class ImageResponseWriterAbstract implements ImageResponseWriter
       writeToStream(raster, defaults, byteStream);
 
 
-      Response.ResponseBuilder response = Response.ok().entity(byteStream.toByteArray())
+      return Response.ok().entity(byteStream.toByteArray())
           .encoding(getResponseMimeType())
           .header("Content-type", getResponseMimeType());
-
-      return response;
 
     }
     catch (final Exception e)
