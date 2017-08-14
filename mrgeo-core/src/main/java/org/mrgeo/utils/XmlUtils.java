f47217c975453f3cea2feb1cc2074cf9711f50ac
Tim Tisler <tim.tisler@digitalglobe.com>
2017-08-14 12:13:54 -0400

    Allow blank "STYLE=" attribute

--- mrgeo-core/src/main/java/org/mrgeo/utils/XmlUtils.java
+++ mrgeo-core/src/main/java/org/mrgeo/utils/XmlUtils.java
@@ -175,7 +175,8 @@ public static Document parseInputStream(InputStream is) throws IOException
     DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
     domFactory.setNamespaceAware(false);
     domFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-    domFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+    // NOTE:  In Java 8, XMLConstants.FEATURE_SECURE_PROCESSING disables XMLConstants.ACCESS_EXTERNAL_DTD
+    // domFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
     DocumentBuilder builder = domFactory.newDocumentBuilder();
     return builder.parse(is);
   }
